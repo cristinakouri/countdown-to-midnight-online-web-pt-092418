1ef57040d45a7237553d611b00256ef91a19760e
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-09-26 20:31:23 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
-def countdown(int)
-  while int >= 0 
-    puts "#{int} SECOND(S)!"
+def countdown(number)
+  while number >= 0 
+    puts "#{int} SECOND (S)!"
     int -= 1 
   end 
   puts "HAPPY NEW YEAR!"
