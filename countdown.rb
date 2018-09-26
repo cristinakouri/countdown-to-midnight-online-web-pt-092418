7d6582f49d2b68d36bf697487cc5a7a123619445
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-09-26 20:35:48 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 def countdown(number)
   while number >=0  
-    puts "#{number} SECOND(S)!"
+    "#{number} SECOND(S)!"
     number -= 1 
   end 
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
 
 countdown(10)
